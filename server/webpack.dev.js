b7f49f33663049af68144dee504b7c9134b11b48
jrsouzadev <jrsouzadev@gmail.com>
2023-01-21 00:57:08 -0300

    Fixed build

--- server/webpack.dev.js
+++ server/webpack.dev.js
@@ -25,32 +25,3 @@ module.exports = {
     clean: true,
   },
 };
-
-/** 
-import path from "path";
-import nodeExternals from "webpack-node-externals";
-
-export default {
-  mode: "development",
-  target: "node",
-  entry: "./src/index.ts",
-  devtool: "inline-source-map",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  externals: [nodeExternals()],
-  output: {
-    filename: "index.js",
-    path: path.resolve("./dist"),
-    clean: true,
-  },
-};*/
--- server/webpack.prod.js
+++ server/webpack.prod.js
@@ -1,5 +1,4 @@
 const path = require("path");
-// const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
   mode: "production",
@@ -23,31 +22,3 @@ module.exports = {
     clean: true,
   },
 };
-
-/**
-import path from "path";
-
-export default {
-  mode: "production",
-  target: "node",
-  entry: "./src/index.ts",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  externals: [nodeExternals()],
-  output: {
-    filename: "index.js",
-    path: path.resolve("./dist"),
-    clean: true,
-  },
-};
-*/
