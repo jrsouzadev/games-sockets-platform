76d5372b8202069c7557ac95517eda1f04006b86
jrsouzadev <jrsouzadev@gmail.com>
2023-01-22 11:01:52 -0300

    Added proxy for socket development

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { io } from "socket.io-client";
+
+const socket = io();
 
 function App() {
+  const [connection, setConnection] = useState(socket.connected);
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log(`Socket connected. Id: ${socket.id}`);
+      socket.emit("I am alive!");
+      setConnection(true);
+    });
+  });
+
   return (
     <div
       style={{
@@ -13,6 +26,9 @@ function App() {
       <header>
         <p style={{ fontSize: "30px" }}>
           <strong>Welcome. Hello world!</strong> by João Ricardo
+        </p>
+        <p style={{ fontSize: "30px" }}>
+          <strong>Socket connected: </strong> {connection ? "ON" : "OFF"}
         </p>
       </header>
     </div>
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,25 +1,37 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path'
+import http from 'http'
+import { Server, Socket } from 'socket.io';
 
-import { generateId } from './utils';
+// import { generateId } from './utils';
 
 dotenv.config();
-
-const app = express();
-
-const port = process.env.PORT || 3333;
-
 const production = process.env.NODE_ENV === 'production'
 
+const expressApp = express();
+const httpServer = http.createServer(expressApp)
+const socketsServer = new Server(httpServer)
+
+const port = production ? process.env.PORT || 3333 : 3333;
+
+
 if (production) {
-  app.use(express.static(path.join(__dirname, 'public')))
+  expressApp.use(express.static(path.join(__dirname, 'public')))
 } else {
-  app.get('/', async (req, res) => {
+  expressApp.get('/', async (req, res) => {
     res.status(200).send('Server running')
   });
 };
 
-app.listen(port, () => {
+socketsServer.on('connection', (socket: Socket) => {
+  console.log(`Socket connected: id ${socket.id}`)
+})
+
+socketsServer.on('disconnect', (socket: Socket) => {
+  console.log(`Socket disconnected: id ${socket.id}`)
+})
+
+httpServer.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
